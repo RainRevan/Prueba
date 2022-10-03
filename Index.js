34812cbb6e0b65e87ed2edded0e0caf56d141514
Javier Alejandro Rincón <110916364+RainRevan@users.noreply.github.com>
2022-10-03 11:16:26 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,4 +1,5 @@
 Let button = document.getElementById(‘BtnRegistrarme’);
 Button.adEventListener(‘click’, function() {
+    window.open("https://www.argar.cat", "Diseño Web", "width=300, height=200")
     Alert(‘Mensaje de Alerta’);
 })
